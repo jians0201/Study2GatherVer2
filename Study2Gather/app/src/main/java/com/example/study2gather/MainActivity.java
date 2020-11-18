1cf5c192167f43cd6ec774483149c731440fc603
Bk49 <ericngyongwei@gmail.com>
2020-11-18 13:00:40 +0800

    Update MainActivity.java

--- Study2Gather/app/src/main/java/com/example/study2gather/MainActivity.java
+++ Study2Gather/app/src/main/java/com/example/study2gather/MainActivity.java
@@ -21,39 +21,31 @@ import com.example.study2gather.ui.profile.ProfileFragment;
 import com.google.android.material.bottomnavigation.BottomNavigationView;
 import com.google.android.material.navigation.NavigationView;
 
-<<<<<<< HEAD:Study2Gather/app/src/main/java/com/example/study2gather/Home.java
-public class Home extends AppCompatActivity implements View.OnClickListener{
-=======
-public class MainActivity extends AppCompatActivity{
->>>>>>> 798ce768c88050904a14f03cf4c879d7726756d6:Study2Gather/app/src/main/java/com/example/study2gather/MainActivity.java
 
+public class MainActivity extends AppCompatActivity/* implements View.OnClickListener*/ {
     private BottomNavigationView navView;
     private NavController navController;
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(Bundle savedInstanceState){
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main_activity);
         navView = findViewById(R.id.nav_view);
         // Passing each menu ID as a set of Ids because each
         // menu should be considered as top level destinations.
-        AppBarConfiguration appBarConfiguration = new AppBarConfiguration.Builder(
-                R.id.navigation_home, R.id.navigation_forum, R.id.navigation_courses, R.id.navigation_messages, R.id.navigation_profile)
-                .build();
+        AppBarConfiguration appBarConfiguration = new AppBarConfiguration.Builder(R.id.navigation_home, R.id.navigation_forum, R.id.navigation_courses, R.id.navigation_messages, R.id.navigation_profile).build();
         navController = Navigation.findNavController(this, R.id.nav_host_fragment);
         NavigationUI.setupActionBarWithNavController(this, navController, appBarConfiguration);
         NavigationUI.setupWithNavController(navView, navController);
     }
-<<<<<<< HEAD:Study2Gather/app/src/main/java/com/example/study2gather/Home.java
 
-     @Override
-    public void onClick(View v) {
-        switch (v.getId()) {
-            case R.id.buttonss:
-                Log.d("testest", "Wtf");
-                break;
-        }
-    }
-=======
->>>>>>> 798ce768c88050904a14f03cf4c879d7726756d6:Study2Gather/app/src/main/java/com/example/study2gather/MainActivity.java
+//    @Override
+//
+//    public void onClick(View v){
+//        switch(v.getId()){
+//            case R.id.buttonss:
+//                Log.d("testest", "Wtf");
+//                break;
+//        }
+//    }
 }
