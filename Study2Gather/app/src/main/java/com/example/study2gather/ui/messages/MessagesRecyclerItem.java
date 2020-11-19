cc3425d9021fb9a6813bc0cfed360bc028d7adc9
Gareth Too <64114942+Gareth2YuSheng@users.noreply.github.com>
2020-11-19 23:25:03 +0800

    Create MessagesRecyclerItem.java

---
+++ Study2Gather/app/src/main/java/com/example/study2gather/ui/messages/MessagesRecyclerItem.java
@@ -0,0 +1,26 @@
+package com.example.study2gather.ui.messages;
+
+import android.net.Uri;
+
+public class MessagesRecyclerItem {
+    String chatTitle, lastMsg;
+    Uri chatPic;
+
+    public MessagesRecyclerItem() {}
+
+    public MessagesRecyclerItem(String chatTitle) {
+        this.chatTitle = chatTitle;
+    }
+
+    public String getChatTitle() { return chatTitle; }
+
+    public void setChatTitle(String chatTitle) { this.chatTitle = chatTitle; }
+
+    public String getLastMsg() { return lastMsg; }
+
+    public void setLastMsg(String lastMsg) { this.lastMsg = lastMsg; }
+
+    public Uri getChatPic() { return chatPic; }
+
+    public void setChatPic(Uri chatPic) { this.chatPic = chatPic; }
+}
