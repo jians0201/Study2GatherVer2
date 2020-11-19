4bff6e66fbde48fe57bb5ec1e489d5f273a7ecfc
Gareth Too <64114942+Gareth2YuSheng@users.noreply.github.com>
2020-11-19 23:42:16 +0800

    a

--- Study2Gather/app/src/main/java/com/example/study2gather/ui/messages/MessagesFragment.java
+++ Study2Gather/app/src/main/java/com/example/study2gather/ui/messages/MessagesFragment.java
@@ -7,16 +7,20 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 import androidx.lifecycle.Observer;
 import androidx.lifecycle.ViewModelProvider;
+import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
 import com.example.study2gather.Chat;
+import com.example.study2gather.Post;
 import com.example.study2gather.R;
+import com.example.study2gather.ui.home.HomeRecylerItemArrayAdapter;
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
@@ -33,6 +37,7 @@ public class MessagesFragment extends Fragment {
     private MessagesViewModel messagesViewModel;
     private FloatingActionButton btnNewMessage;
     private RecyclerView messagesRecyclerView;
+    private View root;
 
     private DatabaseReference chatsRef;
     private FirebaseUser user;
@@ -65,6 +70,32 @@ public class MessagesFragment extends Fragment {
 
         return root;
     }
+
+//    private void setUIRef() {
+//        //Reference of RecyclerView
+//        messagesRecyclerView = root.findViewById(R.id.homePostList);
+//        //Linear Layout Manager
+//        LinearLayoutManager linearLayoutManager = new LinearLayoutManager(getContext(), RecyclerView.VERTICAL, false);
+//        //Set Layout Manager to RecyclerView
+//        messagesRecyclerView.setLayoutManager(linearLayoutManager);
+//        //reverse posts so most recent on top
+////        Collections.reverse(mPosts);
+//
+//        //Create adapter
+//        MessagesRecyclerItemArrayAdapter myRecyclerViewAdapter = new MessagesRecyclerItemArrayAdapter(mChats, new MessagesRecyclerItemArrayAdapter.MyRecyclerViewItemClickListener()
+//        {
+//            //Handling clicks
+//            @Override
+//            public void onItemClicked(Post post)
+//            {
+//                Toast.makeText(getContext(), post.getPostAuthor(), Toast.LENGTH_SHORT).show();
+//            }
+//        });
+//
+//        //Set adapter to RecyclerView
+//        messagesRecyclerView.setAdapter(myRecyclerViewAdapter);
+//    }
+
 
     public void createNewChat() {
         String chatName = "chat"+(maxId+1);
---
+++ Study2Gather/app/src/main/java/com/example/study2gather/ui/messages/MessagesRecyclerItemArrayAdapter.java
@@ -0,0 +1,5 @@
+package com.example.study2gather.ui.messages;
+
+public class MessagesRecyclerItemArrayAdapter {
+
+}
