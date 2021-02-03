a7abe5a5d8e2ec734b87199d8017015f01665097
Jian Sheng <64114913+jians0201@users.noreply.github.com>
2021-02-03 09:28:05 +0800

    fix msg edittext unfocus

--- Study2Gather/app/src/main/java/com/example/study2gather/ui/messages/MessagesChat.java
+++ Study2Gather/app/src/main/java/com/example/study2gather/ui/messages/MessagesChat.java
@@ -10,9 +10,11 @@ import androidx.recyclerview.widget.RecyclerView;
 
 import android.app.Application;
 import android.content.Context;
+import android.graphics.Rect;
 import android.net.Uri;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.MotionEvent;
 import android.view.View;
 import android.view.inputmethod.InputMethodManager;
 import android.widget.Button;
@@ -113,6 +115,24 @@ public class MessagesChat extends AppCompatActivity implements View.OnClickListe
                 }
             }
         });
+    }
+
+    @Override
+    public boolean dispatchTouchEvent(MotionEvent event) {
+        if (event.getAction() == MotionEvent.ACTION_DOWN) {
+            View v = getCurrentFocus();
+            if ( v instanceof EditText) {
+                Rect outRect = new Rect();
+                v.getGlobalVisibleRect(outRect);
+                if (!outRect.contains((int)event.getRawX(), (int)event.getRawY())) {
+                    Log.d("focus", "touchevent");
+                    v.clearFocus();
+                    InputMethodManager imm = (InputMethodManager) getSystemService(Context.INPUT_METHOD_SERVICE);
+                    imm.hideSoftInputFromWindow(v.getWindowToken(), 0);
+                }
+            }
+        }
+        return super.dispatchTouchEvent(event);
     }
 
 
